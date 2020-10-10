9bbaa4dcba5886eb96e48c54bb911b16d3dbfb0e
Armand Fardeau <fardeauarmand@gmail.com>
2020-10-11 01:42:40 +0200

    Register details state

--- assets/details.js
+++ assets/details.js
@@ -1,1 +1,34 @@
+const localStorageKey = "details-state";
+const tagName = "details";
+
+const storedItems = JSON.parse(localStorage.getItem(localStorageKey));
+
+const setItem = (key, value) => {
+    let items = storedItems;
+    if (!items) {
+        items = {};
+    }
+    items[key] = value;
+    localStorage.setItem(localStorageKey, JSON.stringify(items));
+};
+
+const registerItemState = (item) => {
+    if (item.open) {
+        setItem(item.id, "close");
+    } else {
+        setItem(item.id, "open");
+    }
+};
+
+const items = document.getElementsByTagName(tagName);
+
+Array.from(items).forEach((item) => {
+    item.addEventListener('click', registerItemState.bind(null, item));
+});
+
+if (storedItems) {
+    Array.from(Object.keys(storedItems)).forEach((element) => {
+        document.getElementById(element).open = storedItems[element] === "open";
+    });
+}
 
